d3664704f1265200590df4187968f15208e21564
zhijie lee <onezeros.lee@gmail.com>
2012-09-24 22:00:20 +0800

    basic ui for mac

---
+++ Mac/TrackPad/TrackPad/OZLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OZLAppDelegate.h
+//  TrackPad
+//
+//  Created by Lee Zhijie on 9/24/12.
+//  Copyright (c) 2012 Lee Zhijie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OZLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Mac/TrackPad/TrackPad/OZLAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  OZLAppDelegate.m
+//  TrackPad
+//
+//  Created by Lee Zhijie on 9/24/12.
+//  Copyright (c) 2012 Lee Zhijie. All rights reserved.
+//
+
+#import "OZLAppDelegate.h"
+
+@implementation OZLAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
